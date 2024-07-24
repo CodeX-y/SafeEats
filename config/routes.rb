d78f46da3dbf3caacef5bfc1e3616760cb3b0ca7
Yusra A. <84106371+CodeX-y@users.noreply.github.com>
2024-07-24 19:04:12 +0000

    added some routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
+  root to: "users#"
+
   devise_for :users
-  
+
   resources :diet_compliances
   resources :ingredients
   resources :user_diets
   resources :diet_types
 
-  # root to: "home#index"
-  
+  get ":username/diet" => "user_diets#index", as: :user_diet
+  get "diets" => "diet_types#index", as: :diets
+  get ":username/ingredient" => "ingredients#index", as: :ingredient
+
 end
