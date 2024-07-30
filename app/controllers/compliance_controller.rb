363e23f266db483130084cb1fd0e6197b457908f
Yusra Ahmed <yusra245ahmed@gmail.com>
2024-07-29 21:32:15 -0500

    add conditional logic for compliance

--- app/controllers/compliance_controller.rb
+++ app/controllers/compliance_controller.rb
@@ -1,7 +1,13 @@
 class ComplianceController < ApplicationController
   def safe
-    ingredient = params[:ingredient]
-    @found_ingredient = Ingredient.where("LOWER(name) = ?", ingredient.downcase).any?
+    @ingredient_name = params["ingredient"]
+    @ingredient = Ingredient.where("LOWER(name) = ?", @ingredient_name.downcase).first
+
+    vegan_diet = DietType.where({ :name => "vegan" }).first
+    vegetarian_diet = DietType.where({ :name => "vegetarian" }).first
+
+    @vegan_diet_id = vegan_diet ? vegan_diet.id : nil
+    @vegetarian_diet_id = vegetarian_diet ? vegetarian_diet.id : nil
     
     render "compliant/safe"
   end
