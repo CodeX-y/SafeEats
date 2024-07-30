9b1b35b025c3a7249c00624cd96e3f66e905b34d
Yusra Ahmed <yusra245ahmed@gmail.com>
2024-07-29 21:06:26 -0500

    determine that ingredient exists in table

--- app/controllers/compliance_controller.rb
+++ app/controllers/compliance_controller.rb
@@ -1,6 +1,7 @@
 class ComplianceController < ApplicationController
   def safe
-    @ingredient = params[:ingredient]
+    ingredient = params[:ingredient]
+    @found_ingredient = Ingredient.where("LOWER(name) = ?", ingredient.downcase).any?
     
     render "compliant/safe"
   end
