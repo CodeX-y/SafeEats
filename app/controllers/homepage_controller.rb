ee481792c37a7a923c7985aeed735a6292189595
Yusra Ahmed <yusra245ahmed@gmail.com>
2024-08-01 16:47:52 -0500

    added alert in ingredients

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -7,7 +7,7 @@ class HomepageController < ApplicationController
   def update
     current_user.diet_id = params["diet_id"]
     if current_user.save
-      redirect_to(check_ingredient_path)
+      redirect_to check_ingredient_path
     else
       @diet_types = DietType.all
       redirect_to root_path, alert: "Please select your diet before proceeding."
