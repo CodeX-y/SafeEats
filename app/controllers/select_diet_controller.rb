5a45b418fba8fa40984af6dd958c6a60418c526c
Yusra Ahmed <yusra245ahmed@gmail.com>
2024-08-02 22:27:37 -0500

    renamed homepage to select_diet

--- app/controllers/select_diet_controller.rb
+++ app/controllers/select_diet_controller.rb
@@ -1,7 +1,7 @@
-class HomepageController < ApplicationController
+class SelectDietController < ApplicationController
   def edit
     @diet_types = DietType.all
-    render "homepage/edit"
+    render "select_diet/edit"
   end
 
   def update
@@ -13,17 +13,17 @@ class HomepageController < ApplicationController
     if current_user.save
       if current_user.diet_id.present?
         if current_user.diet_id == halal_diet&.id || current_user.diet_id == kosher_diet&.id
-          redirect_to edit_homepage_path, notice: "Halal and Kosher diets are not supported. Please select a different diet."
+          redirect_to edit_select_diet_path, notice: "Halal and Kosher diets are not supported. Please select a different diet."
         else
           redirect_to check_ingredient_path
         end
       else
         @diet_types = DietType.all
-        redirect_to edit_homepage_path, alert: "Please select your diet before proceeding."
+        redirect_to edit_select_diet_path, alert: "Please select your diet before proceeding."
       end
     else
       @diet_types = DietType.all
-      redirect_to edit_homepage_path, alert: "There was an issue saving your selection. Please try again."
+      redirect_to edit_select_diet_path, alert: "There was an issue saving your selection. Please try again."
     end
   end
 
