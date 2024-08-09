d03fd25d4dbd4bd7759ab1fb17ca021f793f225e
Yusra Ahmed <yusra245ahmed@gmail.com>
2024-08-09 15:12:45 -0500

    more superlinter fixes and removal of comments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   config.logger = ActiveSupport::Logger.new(STDOUT)
     .tap  { |logger| logger.formatter = ::Logger::Formatter.new }
     .then { |logger| ActiveSupport::TaggedLogging.new(logger) }
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
   config.action_mailer.perform_caching = false
   config.i18n.fallbacks = true
